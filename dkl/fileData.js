2a322a32af4998222eceb18f60f5295f5aaec667
ธนาวัล บุณสอน <152389095+thanawan230653@users.noreply.github.com>
2025-12-27 17:24:41 +0700

    Update fileData.js

--- dkl/fileData.js
+++ dkl/fileData.js
@@ -5,7 +5,7 @@
 const fileData = `
 2845<PAT Server Team<2025-12-01<https://example.com/file1.zip
 7777<PAT DataCenter<2025-12-05<https://example.com/file2.zip
-8888<Pigcee Lab<2025-12-10<https://example.com/file3.zip
+3333<Pigcee Lab<2025-12-10<https://drive.google.com/file/d/1jU9uIX-hIOV2jdcgFDgBLmHxo7DSvBIR/view?usp=sharing
 `.trim();
 
 // แปลง string เป็น array ของ object
